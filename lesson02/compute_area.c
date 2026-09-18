bb77258593b95a58bfcb49c9bc571b209025b5db
vivian321999 <2486915835@qq.com>
2026-09-18 17:50:32 +0800

    second commit changes

--- lesson02/compute_area.c
+++ lesson02/compute_area.c
@@ -1,25 +1,22 @@
 /*compute area*/
-#include<stdio.h>
- 
+#include <stdio.h>
+
 int main() {
-	/*define vars and constants*/ 
-	double radius, area,volume;
-	const double PI = 3.14;
-	
-	/*input radius*/
-	printf("input radius:");
-	scanf("%lf",&radius);
-	
-	/*compute area*/
-	area = radius * radius * PI;
+    /*define vars and constants*/
+    double radius, area, volume;
+    const double PI = 3.14;
 
+    /*input radius*/
+    printf("input radius:");
+    scanf("%lf", &radius);
 
-	/*compute volume*/
-	volume = (4.0/3.0) * PI * radius * radius * radius;
-	
-	/*output results*/
-	printf("the area for the circle of radius %lf is %lf\n", 
-		radius, area);
-	printf("the volume for the sphere of radius %lf is %lf\n", 
-		radius, volume);
+    /*compute area*/
+    area = radius * radius * PI;
+
+    /*compute volume*/
+    volume = (4.0 / 3.0) * PI * radius * radius * radius;
+
+    /*output results*/
+    printf("the area for the circle of radius %lf is %lf\n", radius, area);
+    printf("the volume for the sphere of radius %lf is %lf\n", radius, volume);
 }
--- lesson02/compute_area_0.c
+++ lesson02/compute_area_0.c
@@ -1,15 +1,14 @@
 /*compute area primary*/
-#include<stdio.h>
- 
+#include <stdio.h>
+
 int main() {
-	/*define vars and constants*/ 
-	double radius = 5; 
-	double area;
-	const double PI = 3.14;
-	/*compute area*/
-	area = radius * radius * PI;
-	
-	/*output results*/
-	printf("the area for the circle of radius %lf is %lf\n", 
-		radius, area);
+    /*define vars and constants*/
+    double radius = 5;
+    double area;
+    const double PI = 3.14;
+    /*compute area*/
+    area = radius * radius * PI;
+
+    /*output results*/
+    printf("the area for the circle of radius %lf is %lf\n", radius, area);
 }
